682ae20adf3185deb7947739d0de9fcaf49be7b2
Franciscosan73 <fsanchezdurand@gmail.com>
2026-02-20 19:57:14 -0300

    Tailwind version corrected, Added responsive and MobileNav, Chart and Animations.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,40 @@
+import HeaderBox from "@/components/HeaderBox"
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
+const Home = () => {
+
+  const loggedIn ={ firstName: 'Francisco', lastName: 'Sanchez', email: 'fsanchezdurand@gmail.com'};
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || 'Guest'}
+          subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+          />
+
+
+        </header>
+                  RECENT TRANSACTIONS
+
+      </div>
+      <RightSidebar 
+      user={loggedIn}
+      transaction={[]}
+      banks={[{}, {}]}
+      />
+    </section>
+  )
+}
+
+export default Home
